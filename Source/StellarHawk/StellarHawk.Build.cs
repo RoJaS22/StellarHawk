b0dc4c5bc104a9fe2472d99bc2a6810634d20f49
Santiago Rojas <rojasolandini@gmail.com>
2026-06-02 19:01:04 -0400

    Organizado las clases en carpetas

--- Source/StellarHawk/StellarHawk.Build.cs
+++ Source/StellarHawk/StellarHawk.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class StellarHawk : ModuleRules
 {
@@ -9,5 +10,18 @@ public class StellarHawk : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
+
+		// Añade esto en el constructor de tu StellarHawk.Build.cs
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Combate"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Core"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "ElementosAmbiente"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Enemigos/EnemigosClases"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Enemigos/Factory"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Enemigos/State"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Items"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Items/PowerUps"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Jugador"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Jugador/Mejoras"));
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "SistemaNiveles"));
 	}
 }
